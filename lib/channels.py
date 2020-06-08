d1a6833bf460c380dfcaf6af94b94ae016d0543b
Blaise Richard <Blaise-Richard@protonmail.com>
2020-06-08 12:39:24 +0200

    Fix: deleting channel bug,  command unused supress

--- lib/channels.py
+++ lib/channels.py
@@ -1,10 +1,11 @@
-import discord
+import discord,re
 import lib.roles
 
 
-async def createChannel(guild,categoryId,name):
+
+async def createChannel(guild,categoryId,name,position):
     category = discord.utils.get(guild.channels, id=categoryId)
-    channel = await guild.create_text_channel(name=name,category=category)
+    channel = await guild.create_text_channel(name=name,category=category,position=position)
     print("channel {} creer".format(name))
     return channel
 
@@ -16,23 +17,24 @@ async def deleteChannel(guild,name):
     else:
         print("channel {} non trouve".format(name))
 
-async def createRoleExclusiveChannel(guild,categoryId,name):
+async def createRoleExclusiveChannel(guild,categoryId,name,position):
     chanName="{}-no-micro".format(name)
     role = await lib.roles.createRole(guild,name)
     discord.utils.get(guild.roles, name=name)
-
     category = discord.utils.get(guild.channels, id=categoryId)
-
     overwrites = {
         guild.default_role: discord.PermissionOverwrite(read_messages=False),
         guild.me : discord.PermissionOverwrite(read_messages=True),
         role: discord.PermissionOverwrite(read_messages=True)
     }
-
     channel = await guild.create_text_channel(name=chanName,category=category, overwrites=overwrites)
+    await channel.edit(position=int(position)) 
     
 async def suppressRoleExclusiveChannel(guild,categoryId,name):
     role = discord.utils.get(guild.roles, name=name)
-    chanName="{}-no-micro".format(name).lower()
+    chanName=name.lower()
+    chanName = re.sub(r'\s+', '-', chanName) 
+    print("chanName : {}".format(chanName))
+    chanName="{}-no-micro".format(chanName)
     await deleteChannel(guild,chanName)
     await lib.roles.deleteRole(guild,name)
--- main.py
+++ main.py
@@ -3,13 +3,22 @@ import os,random as r
 import discord
 from discord.ext import commands
 
+import logging
+
 #local import
 from lib import roles,channels
 
+# logging configuration to get log in a file
+logger = logging.getLogger('discord')
+logger.setLevel(logging.WARNING)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
+#getting the token from the file ".token"
 file= open(".token","r")
 TOKEN=file.read()
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='$')
 
 
 
@@ -19,80 +28,14 @@ async def on_ready():
 	print(f'With ID: {bot.user.id}')
 
 @bot.command()
-async def rand(ctx):
-	await ctx.send(r.randint(1,10))
-
-@bot.command()
+@commands.is_owner()
 async def ping(ctx):
-	await ctx.send("pong")
-
-@bot.command()
-@commands.is_owner()
-async def testRoleExChan(ctx,args):
-	msg= "creation d'un channel {}-no-mic visible uniquement du role {}".format(args,args)
-	await ctx.send(msg)
-	await channels.createRoleExclusiveChannel(ctx.guild,ctx.message.channel.category_id,args)
-
-@bot.command()
-@commands.is_owner()
-async def SupRoleExChan(ctx,args):
-	msg= "suppression du channel {}-no-mic et du role {}".format(args,args)
-	await ctx.send(msg)
-	await channels.suppressRoleExclusiveChannel(ctx.guild,ctx.message.channel.category_id,args)
-
-
-@bot.command()
-@commands.is_owner()
-async def RoleCreate(ctx,args):
-	msg= "creation du role {}".format(args)
-	await ctx.send(msg)
-	await roles.createRole(ctx.channel.guild, args)
-
-@bot.command()
-@commands.is_owner()
-async def RoleSuppress(ctx,args):
-	msg= "supression du role {}".format(args)
-	await ctx.channel.send(msg)
-	await roles.deleteRole(ctx.channel.guild,args)
-
-@bot.command()
-@commands.is_owner()
-async def ChannelCreate(ctx,args):
-	msg= "creation du channel {}".format(args)
-	await ctx.channel.send(msg)
-	await channels.createChannel(ctx.channel.guild,ctx.message.channel.category_id,args)
-	
-@bot.command()
-@commands.is_owner()
-async def ChannelSuppress(ctx,args):
-	msg= "supression du channel {}".format(args)
-	await ctx.channel.send(msg)
-	await channels.deleteChannel(ctx.channel.guild, args)
-
-@bot.command()
-@commands.is_owner()
-async def addRole(ctx,arg1,arg2):
-	msg= "ajout du role {} a l'utilisateur {} ".format(arg2, arg1)
-	member = ctx.guild.get_member_named(arg1)
-	await ctx.channel.send(msg)
-	await roles.addRoleToUser(ctx.channel.guild,member,arg2)
-
-
-@bot.command()
-@commands.is_owner()
-async def removeRole(ctx,arg1,arg2):
-	msg= "suppression du role {} a l'utilisateur {} ".format(arg2, arg1)
-	await ctx.channel.send(msg)
-	member = ctx.guild.get_member_named(arg1)
-	await roles.removeRoleToUser(ctx.channel.guild,member,arg2)
-	
-
+	await ctx.send('latence : {0} ms'.format(round(bot.latency*1000,1)))
 
 @bot.event
 async def on_voice_state_update(member,before,after):
 	print(str(member))
 	if(before.channel != after.channel):
-		
 		if(before.channel != None):
 			if(len(before.channel.members) == 0):
 				print("last to quit the channel")			
@@ -104,7 +47,7 @@ async def on_voice_state_update(member,before,after):
 		if(after.channel != None):
 			if(len(after.channel.members)== 1):
 				print("first in the channel")
-				await channels.createRoleExclusiveChannel(after.channel.guild,after.channel.category_id,after.channel.name)
+				await channels.createRoleExclusiveChannel(after.channel.guild,after.channel.category_id,after.channel.name,after.channel.position)
 
 			await roles.addRoleToUser(after.channel.guild,member,after.channel.name)
 
